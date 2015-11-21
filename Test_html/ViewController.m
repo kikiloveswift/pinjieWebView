c262be83bc7ca342f308bd2f1e682c59e28968b1
Young <konglee1@outlook.com>
2015-11-22 01:57:14 +0800

    push

--- Test_html/ViewController.m
+++ Test_html/ViewController.m
@@ -6,9 +6,18 @@
 //  Copyright © 2015年 tigger. All rights reserved.
 //
 
-#import "ViewController.h"
+#define kWidth [UIScreen mainScreen].bounds.size.width
+#define kHeight [UIScreen mainScreen].bounds.size.height
 
-@interface ViewController ()
+
+#import "ViewController.h"
+#import "YLView.h"
+
+
+@interface ViewController (){
+
+    YLView *ModelView;
+}
 
 @end
 
@@ -17,11 +26,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+
+
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)btnClick:(UIButton *)sender {
+    
+    
+    [UIView animateWithDuration:1 animations:^{
+        
+        ModelView = [[YLView alloc] initWithFrame:CGRectMake(0, 0, kWidth, kWidth)];
+        
+        ModelView.center = self.view.center;
+        
+        ModelView.backgroundColor = [UIColor redColor];
+        
+        ModelView.hidden = NO;
+
+        [self.view addSubview:ModelView];
+        
+    }];
+   
+    
+    
+    
 }
 
 @end
