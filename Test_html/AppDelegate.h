2c7df5aaaffa23db558481b094bd5b79ef74d931
Young <konglee1@outlook.com>
2015-11-21 22:44:30 +0800

    Initial Commit

---
+++ Test_html/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test_html
+//
+//  Created by Young on 15/11/21.
+//  Copyright © 2015年 tigger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
